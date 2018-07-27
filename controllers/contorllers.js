131e314783c6a33d198995e7500db0dae1457b06
Christine Bonk <christinebonk@gmail.com>
2018-07-26 20:58:16 -0400

    added page routes

---
+++ controllers/contorllers.js
@@ -0,0 +1,25 @@
+var burger = require("./../models/superryan.js");
+var express = require("express");
+var app = express();
+
+function routes(app) {
+	app.get("/", function(req, res){
+		res.render("index");
+	});
+
+	app.get("/highscores", function(req, res) {
+		res.render("highscores");
+	});
+
+	app.get("/charactercreate", function(req, res) {
+		res.render("charactercreate");
+	});
+
+	app.get("/game", function(req, res) {
+		res.render("game");
+	});
+
+};
+
+module.exports = routes;
+
