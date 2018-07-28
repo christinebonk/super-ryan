1037d9f1c20d64cd84bf7b945fe24c24dd86fe07
Christine Bonk <christinebonk@gmail.com>
2018-07-28 11:06:08 -0400

    added pages

--- server.js
+++ server.js
@@ -2,12 +2,29 @@ var express = require("express");
 var app = express();
 var port = 3000;
 var path = require("path");
-var routes = require("controllers/contorllers.js");
+var routes = require("./controllers/controllers.js");
+var bodyParser = require("body-parser");
+var exphbs = require("express-handlebars");
 
-app.listen(port,function() {
-	console.log("listening on port: " + port)
-})
-
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
 routes(app);
+
+app.listen(process.env.PORT || port, function(){
+	console.log("Listening on PORT " + port);
+})
+
+
+
+
+
+
+
+
+
+
