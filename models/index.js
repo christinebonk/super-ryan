33c50f07144f9dae91706c35e4decb048a907462
Christine Bonk <christinebonk@gmail.com>
2018-08-07 21:28:01 -0400

    small change

--- models/index.js
+++ models/index.js
@@ -10,7 +10,7 @@ var db = {};
 
 if (process.env.DATABASE_URL) {
   // the application is executed on Heroku ... use the postgres database
-  sequelize = new Sequelize(process.env.DATABASE_URL, {
+  var sequelize = new Sequelize(process.env.DATABASE_URL, {
     dialect:  'postgres',
     protocol: 'postgres',
     logging:  true //false
