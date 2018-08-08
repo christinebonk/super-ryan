a89c56f647b5a829c43c7cce665edbfbc94ced54
Christine Bonk <christinebonk@gmail.com>
2018-08-07 21:26:29 -0400

    changing index.js

--- models/index.js
+++ models/index.js
@@ -4,12 +4,17 @@ var fs = require("fs");
 var path = require("path");
 var Sequelize = require("sequelize");
 var basename = path.basename(__filename);
-var env = process.env.NODE_ENV || "production";
+var env = process.env.NODE_ENV || "development";
 var config = require(__dirname + "/../config/config.json")[env];
 var db = {};
 
-if (config.use_env_variable) {
-  var sequelize = new Sequelize(process.env[config.use_env_variable], config);
+if (process.env.DATABASE_URL) {
+  // the application is executed on Heroku ... use the postgres database
+  sequelize = new Sequelize(process.env.DATABASE_URL, {
+    dialect:  'postgres',
+    protocol: 'postgres',
+    logging:  true //false
+  });
 } else {
   var sequelize = new Sequelize(config.database, config.username, config.password, config);
 }
